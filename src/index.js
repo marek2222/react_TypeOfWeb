a0c7e3f6b8d6686d04c508fc85d1be60d27931b9
marek2222 <marekmusial2@o2.pl>
2018-11-20 10:27:19 +0100

    react_TypeOfWeb: 01_3_2:  Pierwszy komponent React: Renderowanie dwóch lub więcej komponentów

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import * as serviceWorker from './serviceWorker';
 
 
 //ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<MojKomponent />, document.getElementById('root'));
+ReactDOM.render(<div><MojKomponent /><MojKomponent /></div>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
