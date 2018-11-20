7df5b1e569fd76f8f1497f7f202d76ad09fd4549
marek2222 <marekmusial2@o2.pl>
2018-11-20 11:21:02 +0100

    react_TypeOfWeb: 04_2:  Parametryzacja komponentów przez 'props': Domyślne propsy

--- src/komponenty/MojKomponent.js
+++ src/komponenty/MojKomponent.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 // import logo from '../logo.svg';
 // import '../App.css';
 
-function MojKomponent({ tytul, zawartosc }) {
+function MojKomponent({ tytul='Modal', zawartosc }) {
     return (
       <div>
         <dialog open>
           <h1>{tytul}</h1>
           <p>{zawartosc}</p>
+          <footer><a href="https://typeofweb.com/">Tak, typeofweb!</a></footer>
         </dialog>
       </div>
     );
-  }
+}
 
 export default MojKomponent;
