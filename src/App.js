19d17679379e9881c78b8fdb3738700b8ca9901a
marek2222 <marekmusial2@o2.pl>
2018-11-20 15:10:15 +0100

    react_TypeOfWeb: 05_2:  Podział na komponenty: utworzenie  komponentów składających się na stronę

--- src/App.js
+++ src/App.js
@@ -1,28 +1,81 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from '../logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
+function AppHeader() {
     return (
-      <div className="App">
-        <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-                Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Learn React
-            </a>
-        </header>
+      <header className="ui fixed menu">
+        <nav className="ui container">
+          <a href="#" className="header item">
+            <img
+              className="logo"
+              src="https://typeofweb.com/wp-content/uploads/2017/08/cropped-typeofweb_logo-04-white-smaller-1-e1504359870362.png"
+            />
+            Lista kontaktów
+          </a>
+          <div className="header item">
+            <button className="ui button">Dodaj</button>
+          </div>
+        </nav>
+      </header>
+    );
+  }
+  
+  function ContactsList() {
+    return (
+      <ul className="ui relaxed divided list selection">
+        <ContactItem
+          login="typeofweb1"
+          name="Lena"
+          department="JavaScript Developer"
+        />
+        <ContactItem
+          login="typeofweb2"
+          name="Brian"
+          department="Human Resources"
+        />
+        <ContactItem
+          login="typeofweb3"
+          name="Rick"
+          department="QA"
+        />
+      </ul>
+    );
+  }
+  
+  function ContactItem({ login, name, department }) {
+    const imgUrl = `../img/${login}.png`;
+    return (
+      <li className="item">
+        <Avatar login={login} />
+        <div className="content">
+            <h4 className="header">{name}</h4>
+            <div className="description">{department}</div>
+        </div>
+      </li>
+    );
+  }
+  
+  function Avatar({ login }) {
+    // const imgUrl = `../img/${login}.png`;
+    const imgUrl = ''; 
+    if (login.includes('@') ) {
+        imgUrl = imgUrl = `https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50`;
+    } else {
+        imgUrl = `../img/${login}.png`;
+    }
+    return ( <img src={imgUrl} className="ui mini rounded image" alt="" /> );
+  }
+  
+  function App() {
+    return (
+      <div>
+        <AppHeader />
+        <main className="ui main text container">
+            <ContactsList />
+        </main>
       </div>
     );
   }
-}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-import MojKomponent from './komponenty/MojKomponent';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 
-//ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(
-    <div>
-        <MojKomponent  zawartosc='To jest <b>HTML</b>' />
-        {/* <MojKomponent  zawartosc={To jest <b>HTML</b>} /> */}
-        <MojKomponent  zawartosc={<span>To jest <b>HTML</b></span>} />
-    </div>,     
-    document.getElementById('root')
+    <App />
+    ,document.getElementById('app')
 );
 
 // If you want your app to work offline and load faster, you can change
