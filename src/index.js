650b396eab100ea2fc5dd3cbb41b41e13ef20398
marek2222 <marekmusial2@o2.pl>
2018-11-20 09:46:29 +0100

    react_TypeOfWeb: 01_3:  Pierwszy komponent React: MojKomponent

--- src/index.js
+++ src/index.js
@@ -4,7 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function MojKomponent(){
+    return (
+        <div>
+            <h1>Witaj Świecie!</h1>
+            <h2>Przerabiam kurs react.js</h2>
+        </div>
+    );
+}
+
+//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<MojKomponent />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
