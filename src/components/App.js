c3dc908f9c2719efa4971b0149856095159d242f
marek2222 <marekmusial2@o2.pl>
2018-11-22 10:48:38 +0100

    react_TypeOfWeb: 08_1:  Stan - cz. 2

--- src/components/App.js
+++ src/components/App.js
@@ -9,41 +9,46 @@ import ContactsList from './ContactsList';
 class App extends React.Component {
     constructor(){
         super();
-        this.state = {counter: 0, suum:0, counterDouble: 0};
+        this.state = {clicks: 0, suum:0, counterDouble: 0};
         
     }
     render() {
         return (
             <div>
-                <AppHeader />
+                {/* <AppHeader />
                 <ContactsList />
-                <hr/>
+                <hr/> */}
                 <button onClick={this.increment.bind(this)}>+</button>
-                <output>{this.state.counter}</output>
+                <output>{this.state.clicks}</output>
                 <button onClick={this.decrement.bind(this)}>-</button>
-                <br/>
-                <output onDoubleClick={this.double.bind(this)}>Suma kliknięc: {this.state.suum}</output>
-                <br/>
-                <output>Podwojne kliknięcia: {this.state.counterDouble}</output>
             </div>
         );
     }
     increment(){
+        console.log('clicks: było: ', this.state.clicks);
+        console.log('suum: było: ', this.state.suum)
         this.setState({
-            counter: this.state.counter + 1,
-            suum: this.state.suum + 1
-        })
+                clicks: this.state.clicks + 1,
+                suum: this.state.suum + 1
+            }, () => {
+                console.log('clicks: jest: ', this.state.clicks);
+                console.log('suum: jest: ', this.state.suum)
+                console.log('')
+            }
+        )
     }
     decrement(){
+        console.log('clicks: było: ', this.state.clicks);
+        console.log('suum: było: ', this.state.suum)
         this.setState({
-            counter: this.state.counter - 1,
-            suum: this.state.suum + 1
-        })
-    }
-    double(){
-        this.setState({
-            counterDouble: this.state.counterDouble +1
-        })
+                clicks: this.state.clicks - 1,
+                suum: this.state.suum + 1
+            }, () => {
+                console.log('clicks: jest: ', this.state.clicks);
+                console.log('suum: jest: ', this.state.suum)
+                console.log('')
+            }
+        )
     }
 }
 
