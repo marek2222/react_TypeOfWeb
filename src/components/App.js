54f0625c6eca923dc37b8eb2e3df49a06730069c
marek2222 <marekmusial2@o2.pl>
2018-11-28 14:26:32 +0100

    react_TypeOfWeb: 10_2:  Metody cyklu życia komponentu w React.js: cwiczenie - wariant 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,120 @@
+import React, { Component } from 'react';
 import './App.css';
 
-import React, { Component } from 'react';
 
-import AppHeader from './AppHeader';
-import ContactsList from './ContactsList';
-
-
-class App extends React.Component {
-    constructor(){
-        super();
-        this.state = {imie: '', nazwisko: ''};
-    }
-    render() {
+class App extends React.Component{
+    constructor(props){
+        super(props);
+    }    
+    render(){
         return (
-            <div>
-                <AppHeader />
-                <ContactsList />
-                <hr/>
-                Imię:       <input type='text' value={this.state.imie} onInput={this.onInputImieHandler.bind(this)}></input>
-                <br/>
-                Nazwisko: <input type='text' value={this.state.nazwisko} onInput={this.onInputNazwiskoHandler.bind(this)}></input>
-                <br/>
-                <label>Wpisałeś: {this.state.imie} {this.state.nazwisko}</label>
+            <main className="ui main container">
+                <Parent />
+            </main>
+        );
+    }
+}
+
+class Parent extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            parentNumber : 0,
+            sentNumber: 0
+        };
+    }
+    sendFarAway(){
+        this.setState({
+           sentNumber: this.state.parentNumber 
+        });
+    }
+
+    updateInput(e){
+        this.setState({
+            parentNumber : parseInt(e.target.value)
+        });
+    }
+
+    render(){
+        return (
+            <div className="ui container">
+                <div className="ui right labeled left icon input">
+                    <i className="tags icon"></i>
+                    <input type="number" placeholder="Podaj liczbę" value={this.state.parentNumber} onChange={this.updateInput.bind(this)} />
+                    <a className="ui tag label" onClick={this.sendFarAway.bind(this)} >Przekaż</a>
+                </div>
+                <br /> <br />
+
+                <SweetChild liczba={this.state.sentNumber} />
             </div>
         );
     }
-    onInputImieHandler(event){
-        this.setState({ imie: event.target.value })
+}
+
+class SweetChild extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            fromParent: parseInt(props.liczba)
+        };
     }
-    onInputNazwiskoHandler(event){
-        this.setState({  nazwisko: event.target.value })
+
+    componentWillReceiveProps(nextProps){
+        if(this.props.liczba !== nextProps.liczba){
+            this.setState({
+               fromParent: parseInt(nextProps.liczba)
+            });
+        }
+    }
+
+    incrementValue(){
+        this.setState({
+            fromParent: this.state.fromParent + 1
+        });
+    }
+
+    decrementValue(){
+        this.setState({
+            fromParent: this.state.fromParent -1
+        });
+    }
+
+    render(){
+        return (
+           <div className="ui card">
+                <div className="content">
+                    <div className="header">
+                        Wartość przekazana od rodzica: <br />
+                        <div className="ui segment">
+                            <h1 className="ui center aligned">{this.state.fromParent}</h1>
+                        </div>
+                        
+                        <br />
+                    </div>
+                    <div className="extra content">
+                        <div className="ui buttons fluid">
+                          <button className="ui negative button" onClick={this.decrementValue.bind(this)}>zmniejsz</button>
+                          <div className="or" data-text="lub"></div>
+                          <button className="ui positive button" onClick={this.incrementValue.bind(this)} >zwiększ</button>
+                        </div>
+                    </div>
+                    
+                </div>
+                
+           </div>
+        );
     }
 }
 
 export default App;
+
+
+// Rodzic pozwala na ustawienie (input + button + onClick+ setState) jakiejś wartości liczbowej
+//   i przekazuje ją do dziecka jako props.
+// Dziecko ma początkowo wyświetlać liczbę podaną od rodzica, a dodatkowo ma umożliwiać
+//   zwiększanie i zmniejszanie tej liczby (button + onClick + setState).
+// W momencie gdy rodzic ustawi liczbę, dziecko powinno zresetować swój stan 
+//    do podanej liczby (componentWillReceiveProps).
+
+
