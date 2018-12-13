29c0051d02d49cda19ff14edb98e41c81ce13860
marek2222 <marekmusial2@o2.pl>
2018-12-13 07:20:40 +0100

    react_TypeOfWeb: 18_3:  Testowanie aplikacji React.js — podstawy Enzyme - pkody z filtrowania statycznej listy

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {    shallow} from 'enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
@@ -7,3 +8,30 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow} from 'enzyme';
+import App from './App';
+import UsersList from './UsersList';
+
+it('renders without crashing', () => {
+    shallow( <App /> );
+});
+
+it('includes input', () => {
+    const app = shallow( <App /> );
+    expect(app.containsMatchingElement( <input /> )).toEqual(true)
+});
+
+it('includes UsersList', () => {
+    const app = shallow( <App /> );
+    expect(app.containsMatchingElement( <UsersList /> )).toEqual(true)
+});
+
+it('passes all users to the UsersList', () => {
+    const app = shallow( <App /> );
+    expect(app.find('UsersList').prop('users')).toEqual(['Michal', 'Kasia', 'Jacek', 'Marta', 'Tomek', 'Ania']);
+})
