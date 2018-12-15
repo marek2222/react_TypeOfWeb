43afab81207d01887813aef94646ada4cc9039fe
marek2222 <marekmusial2@o2.pl>
2018-12-15 23:59:25 +0100

    react_TypeOfWeb: 21_3:  Usunięcie plików z poprzednich gałęzi

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import { App } from './App';
 import { MyControlledForm  } from './MyControlledForm';
 import * as serviceWorker from './serviceWorker';
 
