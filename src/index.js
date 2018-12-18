0a140f634a6ab21d17741caa99ab7caf7af46b65
marek2222 <marekmusial2@o2.pl>
2018-12-18 15:48:40 +0100

    react_TypeOfWeb: 21_4:  Formularze w React.js — niekontrolowane komponenty

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import './index.css';
-import { MyControlledForm  } from './MyControlledForm';
+import { MyControlledForm  } from './MyUnControlledForm';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render( <MyControlledForm /> , document.getElementById('root'));
